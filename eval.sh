16cbee8243b2ba716a9267b045b2a49aabdd43f3
syberspaz <syberspaz@gmail.com>
2021-06-09 08:52:30 -0400

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -3,7 +3,7 @@
 # e.g. checkpoint_dir=../log/cscd/noBN_L1_sd100_B16/
 
 expm=$1
-sampledir=`realpath sample/$expm`
+sampledir=/content/RESULTS
 echo "sampledir: "$sampledir
 time=`date +"%m-%d-%H-%M"`
 logfile=${sampledir}/log_$time.txt
@@ -17,9 +17,9 @@ unbuffer python evaluate.py \
     --dtnet=hypercolumn \
     --sample_dir=$sampledir \
     --checkpoint_dir=../log/cscd/noBN_L1_sd100_B16/ \
-    --csv_path=../dataset/test.csv \
+    --csv_path=/content/sample_data/test.csv \
     --low_thres=500 \
     --up_thres=3000 \
-    --image_size=400 \
+    --image_size=1920 \
     --gpu \
 2>&1 | tee $logfile
